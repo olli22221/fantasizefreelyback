a6ad862c00d15be386413dc8d6717040bd70427c
olli <pohloliq@hu-berlin.de>
2022-09-17 17:39:00 +0200

    basicVersionIntegration

--- server.py
+++ server.py
@@ -18,6 +18,7 @@ metrics = PrometheusMetrics(app)
 
 SECRET_KEY = os.getenv("MY_SECRET")
 USER_DIR = os.getenv("USER_DIR")
+USER_DIR_BASIC = os.getenv("USER_DIR_BASIC")
 
 metrics.info('app_info', 'Application info', version='1.0.3')
 
@@ -37,28 +38,12 @@ def get_db_connection():
     conn.row_factory = sqlite3.Row
     return conn
 
+def get_db_connection_basic():
+    conn = sqlite3.connect('dbBasic.sqlite')
+    conn.row_factory = sqlite3.Row
+    return conn
 
 
-@app.route('/upload', methods=['POST'])
-def upload_file():
-    if request.method == 'POST':
-        conn = get_db_connection()
-        f = request.files['file']
-        jwtToken = request.form['jwtToken']
-        count = request.form['count']
-        try:
-            decodedToken = jwt.decode(jwtToken, SECRET_KEY, algorithms=["HS256"])
-        except:
-            return "JWT Token expired", 401
-        subjectId = decodedToken['id']
-        composition_uuid = str(uuid.uuid4())
-        midfilepath = "/home/olli/midiDataSubjects/" + subjectId + "/trial_" + count + ".mid"
-        f.save(midfilepath)
-        result = run(os.getcwd()+"/RhythmCat.exe", midfilepath)
-        conn.execute("INSERT INTO compositions (id,fk,filepath) VALUES( ?,?,?)", (composition_uuid, subjectId, midfilepath))
-        conn.commit()
-        conn.close()
-        return Response(result, status=200)
 
 
 
@@ -85,6 +70,34 @@ def startApp():
         os.mkdir(os.getcwd()+"/../userData/"+ subject_uuid + "/compositions")
 
         encoded_jwt = jwt.encode({"id": subject_uuid,"exp": datetime.datetime.now(tz=datetime.timezone.utc) + datetime.timedelta(seconds=30000)}, SECRET_KEY, algorithm="HS256")
+        print(encoded_jwt)
+        return jsonify(encoded_jwt), 200
+
+
+@app.route('/startBasic', methods=['POST'])
+@common_counter
+def startAppBasic():
+    if request.method == 'POST':
+        user = request.json['data']
+        basic = request.json['basic']
+        conn = get_db_connection_basic()
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM subject WHERE user=?", (user,))
+        row = cur.fetchone()
+        if row is not None:
+            return "There already exists such a username", 408
+        subject_uuid = str(uuid.uuid4())
+
+        conn.execute("""INSERT INTO subject (id,user,basic) VALUES(?,?,?);""", (subject_uuid, user, basic))
+        conn.commit()
+        conn.close()
+        os.mkdir(os.getcwd() + "/../userDataBasic/" + subject_uuid)
+        os.mkdir(os.getcwd() + "/../userDataBasic/" + subject_uuid + "/generatedMelodies")
+        os.mkdir(os.getcwd() + "/../userDataBasic/" + subject_uuid + "/compositions")
+
+        encoded_jwt = jwt.encode({"id": subject_uuid,
+                                  "exp": datetime.datetime.now(tz=datetime.timezone.utc) + datetime.timedelta(
+                                      seconds=30000)}, SECRET_KEY, algorithm="HS256")
         print(encoded_jwt)
         return jsonify(encoded_jwt), 200
 
@@ -120,6 +133,23 @@ def runRNN():
         subjectId = decodedToken['id']
 
         UserPath = USER_DIR + subjectId + "/generatedMelodies"
+        response = runRnn(data, UserPath,meter,temperature)
+        return Response(response, status=200)
+@app.route('/runRNNBasic', methods=['POST'])
+@common_counter
+def runRNNBasic():
+    if request.method == 'POST':
+        data = request.json['data']
+        meter = request.json['meter']
+        jwtToken = request.json['jwtToken']
+        temperature = request.json['temperature']
+        try:
+            decodedToken = jwt.decode(jwtToken, SECRET_KEY, algorithms=["HS256"])
+        except:
+            return "JWT Token expired", 401
+        subjectId = decodedToken['id']
+
+        UserPath = USER_DIR_BASIC + subjectId + "/generatedMelodies"
         response = runRnn(data, UserPath,meter,temperature)
         return Response(response, status=200)
 
@@ -247,6 +277,101 @@ def submitComposition():
         encoded = base64.b64encode(im_bytes).decode("utf8")
         response['musicatPNG'] = encoded
 
+
+        # store values in db
+        # store composition in db
+        # run musicat and produce the png
+        # return also the results from musicats computation
+        response = json.dumps(response)
+        return Response(response, status=200)
+
+
+@app.route('/submitCompositionBasic', methods=['POST'])
+@common_counter
+def submitCompositionBasic():
+    if request.method == 'POST':
+        '''#conn = get_db_connection()
+
+        #jwtToken = request.form['jwtToken']
+        #count = request.form['count']
+        #try:
+           # decodedToken = jwt.decode(jwtToken, SECRET_KEY, algorithms=["HS256"])
+        except:
+            return "JWT Token expired", 401
+        subjectId = decodedToken['id']
+        composition_uuid = str(uuid.uuid4())
+        '''
+        # composition = request.form['composition']
+
+        # result = run(os.getcwd()+"/RhythmCat.exe", composition)
+        # conn.execute("INSERT INTO compositions (id,fk,filepath) VALUES( ?,?,?)", (composition_uuid, subjectId, midfilepath))
+        # conn.commit()
+        # conn.close()
+        jwtToken = request.json['jwtToken']
+
+        try:
+            decodedToken = jwt.decode(jwtToken, SECRET_KEY, algorithms=["HS256"])
+        except:
+            return "JWT Token expired", 401
+        conn = get_db_connection_basic()
+        cur = conn.cursor()
+        composition_uuid = str(uuid.uuid4())
+        subjectId = decodedToken['id']
+        data = request.json['data']
+        pathToCompositions = USER_DIR_BASIC + subjectId + "/compositions/"
+
+        userCompositions = []
+        for file in os.listdir(pathToCompositions):
+            with open(pathToCompositions + file + "/compositionData.json") as tmpFile:
+                jsonData = json.load(tmpFile)
+                jsonData = json.loads(jsonData)
+
+                userCompositions.append(jsonData)
+
+        orig, flex, fluency = calculateCreativityScores(data, userCompositions)
+
+        cur.execute("SELECT * FROM compositions WHERE id=?", (composition_uuid,))
+        row = cur.fetchone()
+        while row is not None:
+            composition_uuid = str(uuid.uuid4())
+            cur.execute("SELECT * FROM compositions WHERE id=?", (composition_uuid,))
+            row = cur.fetchone()
+        os.mkdir(USER_DIR_BASIC + subjectId + "/compositions/" + composition_uuid)
+        os.mkdir(USER_DIR_BASIC + subjectId + "/compositions/" + composition_uuid + "/musicatPNG")
+        pathToImageDir = USER_DIR_BASIC + subjectId + "/compositions/" + composition_uuid + "/musicatPNG/"
+        jsonComposition = json.dumps(data)
+        pathToComposition = USER_DIR_BASIC + subjectId + "/compositions/" + composition_uuid + "/compositionData.json"
+
+        with open(pathToComposition, 'w') as f:
+            json.dump(jsonComposition, f)
+        now = datetime.datetime.now()
+        date_time = now.strftime("%m/%d/%Y, %H:%M:%S")
+
+        conn.execute("""INSERT INTO compositions (id,fk,filepath,pathToComposition,timestamp) VALUES( ?,?,?,?,?);""",
+                     (composition_uuid, subjectId, pathToImageDir, pathToComposition, date_time))
+        conn.commit()
+
+        print(pathToImageDir)
+        result = run(os.getcwd() + "/RhythmCat.exe", data, pathToImageDir)
+        analogies = result["analogies"]
+        groups = result["groups"]
+        strengths = result["strengths"]
+
+        totalResult, musicat = computeTotalResult(orig, flex, fluency, analogies, groups, data)
+        response = {}
+
+        score_uuid = str(uuid.uuid4())
+        conn.execute(
+            """INSERT INTO Scores (id,composition,fluency,flexability,originality,total,musicat)VALUES( ?,?,?,?,?,?,?);""",
+            (score_uuid, composition_uuid, fluency, flex, orig, totalResult, musicat))
+        conn.commit()
+
+        response['totalScore'] = totalResult
+
+        with open(pathToImageDir + "out.png", 'rb') as musicatImage:
+            im_bytes = musicatImage.read()
+        encoded = base64.b64encode(im_bytes).decode("utf8")
+        response['musicatPNG'] = encoded
 
         # store values in db
         # store composition in db
